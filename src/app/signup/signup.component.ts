360b739fa95659c8acbeeb620f17c715a919d336
singhcodex <emosingh81@gmail.com>
2023-02-22 14:40:32 +0100

    serive singup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 
 import * as firebase from 'firebase/app';
 import * as auth from 'firebase/auth';
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
@@ -13,22 +15,44 @@ export class SignupComponent{
 
   myForm!: FormGroup;
 
-  constructor(public fb: FormBuilder){
+  constructor(public fb: FormBuilder, private authService: AuthService, private router:Router){
     this.myForm = this.fb.group({
       firstName: new FormControl(null, Validators.required),
       lastName: new FormControl(null, Validators.required),
       email: new FormControl(null, Validators.required),
       password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
       confirmPassword:new FormControl(null, Validators.required)
+    },{
+      validator: this.checkIfMatchingPasswords("password","confirmPassword")
     })
+  }
+
+  checkIfMatchingPasswords(passwordKey:string, confirmPasswordKey:string){
+    return (group: FormGroup) => {
+      let password = group.controls[passwordKey];
+      let confirmPassword = group.controls[confirmPasswordKey];
+
+      if(password.value == confirmPassword.value){
+        return;
+      }else{
+        confirmPassword.setErrors({
+          notEqualToPassword: true
+        })
+      }
+
+    }
   }
 
   onSubmit(myForm:any){
     //console.log(this.myForm.value);
     let email:string = myForm.value.email;
     let password:string = myForm.value.password;
-
     let firstName:string = myForm.value.firstName;
     let lastName:string = myForm.value.lastName;
+
+    this.authService.signUp(email,password).subscribe(() => {
+      console.log("signup successful");
+    })
+
   }
 }
